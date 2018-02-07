43001376ef62c4784700456c64f4ecce9dca79b5
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-02-07 13:13:37 -0500

    initial commit
    add all code from day 1

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+
+  root 'post#index'
+  
+  get 'index' => 'practice#index'
+
+  post 'index' => 'practice#index'
+
+  get 'about' => 'practice#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
